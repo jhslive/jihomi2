c6853ea19f69862467063a224efa955588f20f1b
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-23 09:15:15 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import html2canvas from "html2canvas";
 
 function App() {
   const [coin, setCoin] = useState("BTCUSDT");
-  const [entryPrice, setEntryPrice] = useState("15922");
-  const [closingPrice, setClosingPrice] = useState("16324");
+  const [entryPrice, setEntryPrice] = useState("15879");
+  const [closingPrice, setClosingPrice] = useState("15625");
   const [isLong, setIsLong] = useState(true);
   const [numsLoc, setNumsLoc] = useState([227, 570, 570]);
   const [numLocLR, setNumLocLR] = useState(1000);
   const [leverage, setLeverage] = useState("75.00");
-  const [date, setdate] = useState("11/21/2022, 13:15:30");
+  const [date, setdate] = useState("11/22/2022, 16:02:30");
   const [result, setResult] = useState(
     ((closingPrice / entryPrice - 1) * 75 * 100).toFixed(2)
   );
