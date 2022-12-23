fbc1d1b65b382b5b8cf4b2b70606e65f06dbef3c
jhslive <111948596+jhslive@users.noreply.github.com>
2022-12-23 10:33:24 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [coin, setCoin] = useState("DOGEUSDT");
   const [entryPrice, setEntryPrice] = useState("0.07425");
   const [closingPrice, setClosingPrice] = useState("0.07342");
-  const [isLong, setIsLong] = useState(true);
+  const [isLong, setIsLong] = useState(false);
   const [numsLoc, setNumsLoc] = useState([0, 0, 0]);
   const [numLocLR, setNumLocLR] = useState(0);
   const [leverage, setLeverage] = useState("70.00");
