424f3593a429a73f97b5e5bdf6342985ac0e560d
jhslive <111948596+jhslive@users.noreply.github.com>
2023-01-03 10:00:14 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ function App() {
 
             }}
           >
-            {leverage.toString(2)}X
+            {Number(leverage).toFixed(2)}X
           </div>
           <div
             style={{
